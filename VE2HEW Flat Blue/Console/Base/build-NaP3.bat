7c3a3aef49c353995b2379c9b1c1dcd131821a06
Anthony Di Iorio <anthony@diiorio.me>
2019-12-17 17:46:21 -0500

    Added NaP3 Support

---
+++ VE2HEW Flat Blue/Console/Base/build-NaP3.bat
@@ -0,0 +1,222 @@
+set skinpath=.\..\
+
+Set console=Console.png
+Set picDisplay=picDisplay.png
+
+set up1=longledbttn-0.png
+set dn1=longledbttn-1.png
+set slideback=slider_back.png
+set slidehead=slider_head.png
+
+copy %console% %skinpath%Console.png
+copy %picDisplay% %skinpath%picDisplay.png
+
+copy %up1% %skinpath%btnBandHF-0.png
+copy %dn1% %skinpath%btnBandHF-1.png
+copy %up1% %skinpath%btnBandVHF-0.png
+copy %dn1% %skinpath%btnBandVHF-1.png
+copy %up1% %skinpath%btnDisplayPanCenter-0.png
+copy %dn1% %skinpath%btnDisplayPanCenter-1.png
+copy %up1% %skinpath%btnFilterGroup-0.png
+copy %dn1% %skinpath%btnFilterGroup-1.png
+
+copy %up1% %skinpath%btnTuneStepChangeSmaller-0.png
+copy %dn1% %skinpath%btnTuneStepChangeSmaller-1.png
+copy %up1% %skinpath%btnTuneStepChangeLarger-0.png
+copy %dn1% %skinpath%btnTuneStepChangeLarger-1.png
+
+copy %up1% %skinpath%btnZeroBeat-0.png
+copy %dn1% %skinpath%btnZeroBeat-1.png
+copy %up1% %skinpath%btnVFOBtoA-0.png
+copy %dn1% %skinpath%btnVFOBtoA-1.png
+copy %up1% %skinpath%btnVFOAtoB-0.png
+copy %dn1% %skinpath%btnVFOAtoB-1.png
+copy %up1% %skinpath%btnVFOSwap-0.png
+copy %dn1% %skinpath%btnVFOSwap-1.png
+
+copy %up1% %skinpath%chkANF-0.png
+copy %dn1% %skinpath%chkANF-1.png
+copy %up1% %skinpath%chkBIN-0.png
+copy %dn1% %skinpath%chkBIN-1.png
+copy %up1% %skinpath%chkDisplayAVG-0.png
+copy %dn1% %skinpath%chkDisplayAVG-1.png
+copy %up1% %skinpath%chkDisplayFixed-0.png
+copy %dn1% %skinpath%chkDisplayFixed-1.png
+copy %up1% %skinpath%chkDisplayPeak-0.png
+copy %dn1% %skinpath%chkDisplayPeak-1.png
+
+copy %up1% %skinpath%chkDSPNB2-0.png
+copy %dn1% %skinpath%chkDSPNB2-1.png
+copy %up1% %skinpath%chkEnableSubRX-0.png
+copy %dn1% %skinpath%chkEnableSubRX-1.png
+copy %up1% %skinpath%chkMUT-0.png
+copy %dn1% %skinpath%chkMUT-1.png
+copy %up1% %skinpath%chkNB-0.png
+copy %dn1% %skinpath%chkNB-1.png
+copy %up1% %skinpath%chkNR-0.png
+copy %dn1% %skinpath%chkNR-1.png
+copy %up1% %skinpath%chkRXEQ-0.png
+copy %dn1% %skinpath%chkRXEQ-1.png
+copy %up1% %skinpath%chkSquelch-0.png
+copy %dn1% %skinpath%chkSquelch-1.png
+copy %up1% %skinpath%chkVFOSplit-0.png
+copy %dn1% %skinpath%chkVFOSplit-1.png
+
+copy %slideback% %skinpath%ptbMainRXGain-back.png
+copy %slidehead% %skinpath%ptbMainRXGain-head.png
+copy %slideback% %skinpath%ptbRF-back.png
+copy %slidehead% %skinpath%ptbRF-head.png
+copy %slideback% %skinpath%ptbSpan-back.png
+copy %slidehead% %skinpath%ptbSpan-head.png
+copy %slideback% %skinpath%ptbSquelch-back.png
+copy %slidehead% %skinpath%ptbSquelch-head.png
+copy %slideback% %skinpath%ptbSubRXGain-back.png
+copy %slidehead% %skinpath%ptbSubRXGain-head.png
+
+copy %up1% %skinpath%radBand2-0.png
+copy %up1% %skinpath%radBand6-0.png
+copy %up1% %skinpath%radBand10-0.png
+copy %up1% %skinpath%radBand12-0.png
+copy %up1% %skinpath%radBand15-0.png
+copy %up1% %skinpath%radBand17-0.png
+copy %up1% %skinpath%radBand20-0.png
+copy %up1% %skinpath%radBand30-0.png
+copy %up1% %skinpath%radBand40-0.png
+copy %up1% %skinpath%radBand60-0.png
+copy %up1% %skinpath%radBand80-0.png
+copy %up1% %skinpath%radBand160-0.png
+copy %up1% %skinpath%radBandGEN-0.png
+
+copy %dn1% %skinpath%radBand2-1.png
+copy %dn1% %skinpath%radBand6-1.png
+copy %dn1% %skinpath%radBand10-1.png
+copy %dn1% %skinpath%radBand12-1.png
+copy %dn1% %skinpath%radBand15-1.png
+copy %dn1% %skinpath%radBand17-1.png
+copy %dn1% %skinpath%radBand20-1.png
+copy %dn1% %skinpath%radBand30-1.png
+copy %dn1% %skinpath%radBand40-1.png
+copy %dn1% %skinpath%radBand60-1.png
+copy %dn1% %skinpath%radBand80-1.png
+copy %dn1% %skinpath%radBand160-1.png
+copy %dn1% %skinpath%radBandGEN-1.png
+
+copy %up1% %skinpath%radBandVHF10-0.png
+copy %up1% %skinpath%radBandVHF9-0.png
+copy %up1% %skinpath%radBandVHF8-0.png
+copy %up1% %skinpath%radBandVHF7-0.png
+copy %up1% %skinpath%radBandVHF6-0.png
+copy %up1% %skinpath%radBandVHF5-0.png
+copy %up1% %skinpath%radBandVHF4-0.png
+copy %up1% %skinpath%radBandVHF3-0.png
+copy %up1% %skinpath%radBandVHF2-0.png
+copy %up1% %skinpath%radBandVHF1-0.png
+copy %up1% %skinpath%radBandVHF0-0.png
+
+copy %dn1% %skinpath%radBandVHF10-1.png
+copy %dn1% %skinpath%radBandVHF9-1.png
+copy %dn1% %skinpath%radBandVHF8-1.png
+copy %dn1% %skinpath%radBandVHF7-1.png
+copy %dn1% %skinpath%radBandVHF6-1.png
+copy %dn1% %skinpath%radBandVHF5-1.png
+copy %dn1% %skinpath%radBandVHF4-1.png
+copy %dn1% %skinpath%radBandVHF3-1.png
+copy %dn1% %skinpath%radBandVHF2-1.png
+copy %dn1% %skinpath%radBandVHF1-1.png
+copy %dn1% %skinpath%radBandVHF0-1.png
+
+copy %up1% %skinpath%radFilter1-0.png
+copy %up1% %skinpath%radFilter2-0.png
+copy %up1% %skinpath%radFilter3-0.png
+copy %up1% %skinpath%radFilter4-0.png
+copy %up1% %skinpath%radFilter5-0.png
+copy %up1% %skinpath%radFilter6-0.png
+copy %up1% %skinpath%radFilter7-0.png
+copy %up1% %skinpath%radFilterVar-0.png
+
+copy %dn1% %skinpath%radFilter1-1.png
+copy %dn1% %skinpath%radFilter2-1.png
+copy %dn1% %skinpath%radFilter3-1.png
+copy %dn1% %skinpath%radFilter4-1.png
+copy %dn1% %skinpath%radFilter5-1.png
+copy %dn1% %skinpath%radFilter6-1.png
+copy %dn1% %skinpath%radFilter7-1.png
+copy %dn1% %skinpath%radFilterVar-1.png
+
+copy %up1% %skinpath%radModeLSB-0.png
+copy %dn1% %skinpath%radModeLSB-1.png
+copy %up1% %skinpath%radModeCWL-0.png
+copy %dn1% %skinpath%radModeCWL-1.png
+copy %up1% %skinpath%radModeUSB-0.png
+copy %dn1% %skinpath%radModeUSB-1.png
+copy %up1% %skinpath%radModeFMN-0.png
+copy %dn1% %skinpath%radModeFMN-1.png
+copy %up1% %skinpath%radModeDIGL-0.png
+copy %dn1% %skinpath%radModeDIGL-1.png
+copy %up1% %skinpath%radModeDIGU-0.png
+copy %dn1% %skinpath%radModeDIGU-1.png
+copy %up1% %skinpath%radModeCWU-0.png
+copy %dn1% %skinpath%radModeCWU-1.png
+copy %up1% %skinpath%radModeSAM-0.png
+copy %dn1% %skinpath%radModeSAM-1.png
+copy %up1% %skinpath%radModeAM-0.png
+copy %dn1% %skinpath%radModeAM-1.png
+
+copy %up1% %skinpath%radModeK3AM-0.png
+copy %dn1% %skinpath%radModeK3AM-1.png
+copy %up1% %skinpath%radModeK3CW-0.png
+copy %dn1% %skinpath%radModeK3CW-1.png
+copy %up1% %skinpath%radModeK3DATA-0.png
+copy %dn1% %skinpath%radModeK3DATA-1.png
+copy %up1% %skinpath%radModeK3FM-0.png
+copy %dn1% %skinpath%radModeK3FM-1.png
+copy %up1% %skinpath%radModeK3LSB-0.png
+copy %dn1% %skinpath%radModeK3LSB-1.png
+copy %up1% %skinpath%radModeK3USB-0.png
+copy %dn1% %skinpath%radModeK3USB-1.png
+
+copy %up1% %skinpath%radModeOAM-0.png
+copy %dn1% %skinpath%radModeOAM-1.png
+copy %up1% %skinpath%radModeOUCW-0.png
+copy %dn1% %skinpath%radModeOUCW-1.png
+copy %up1% %skinpath%radModeOLCW-0.png
+copy %dn1% %skinpath%radModeOLCW-1.png
+copy %up1% %skinpath%radModeOFSK-0.png
+copy %dn1% %skinpath%radModeOFSK-1.png
+copy %up1% %skinpath%radModeOFM-0.png
+copy %dn1% %skinpath%radModeOFM-1.png
+copy %up1% %skinpath%radModeOLSB-0.png
+copy %dn1% %skinpath%radModeOLSB-1.png
+copy %up1% %skinpath%radModeOUSB-0.png
+copy %dn1% %skinpath%radModeOUSB-1.png
+
+copy %up1% %skinpath%radModePKT-0.png
+copy %dn1% %skinpath%radModePKT-1.png
+copy %up1% %skinpath%radModeRTTY-0.png
+copy %dn1% %skinpath%radModeRTTY-1.png
+copy %up1% %skinpath%radModeYAMFM-0.png
+copy %dn1% %skinpath%radModeYAMFM-1.png
+copy %up1% %skinpath%radModeYCW-0.png
+copy %dn1% %skinpath%radModeYCW-1.png
+copy %up1% %skinpath%radModeYLSB-0.png
+copy %dn1% %skinpath%radModeYLSB-1.png
+copy %up1% %skinpath%radModeYUSB-0.png
+copy %dn1% %skinpath%radModeYUSB-1.png
+
+copy %up1% %skinpath%radMode990PSK-0.png
+copy %dn1% %skinpath%radMode990PSK-1.png
+copy %up1% %skinpath%radMode990FSK-0.png
+copy %dn1% %skinpath%radMode990FSK-1.png
+copy %up1% %skinpath%radMode990FM-0.png
+copy %dn1% %skinpath%radMode990FM-1.png
+copy %up1% %skinpath%radMode990AM-0.png
+copy %dn1% %skinpath%radMode990AM-1.png
+copy %up1% %skinpath%radMode990CW-0.png
+copy %dn1% %skinpath%radMode990CW-1.png
+copy %up1% %skinpath%radMode990CWR-0.png
+copy %dn1% %skinpath%radMode990CWR-1.png
+copy %up1% %skinpath%radMode990LSB-0.png
+copy %dn1% %skinpath%radMode990LSB-1.png
+copy %up1% %skinpath%radMode990USB-0.png
+copy %dn1% %skinpath%radMode990USB-1.png
+
